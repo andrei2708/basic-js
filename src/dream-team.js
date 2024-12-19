14d8478d99cac69bcb11809aeefb9cc8b1990938
Andrei <apavlukovich@gmail.com>
2024-12-19 23:48:09 +0300

    feat: add count cat and dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let dreamTeam = [];
+  for (let i = 0; i < members.length; i += 1) {
+    if (typeof members[i] === 'string') {
+      dreamTeam.push(members[i].trim().charAt(0).toUpperCase());
+    }
+  }
+  return dreamTeam.sort().join('');
 }
 
 module.exports = {
